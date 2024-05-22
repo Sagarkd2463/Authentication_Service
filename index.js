b6dc86c42984751ad12976bce072e651b4f796b2
Sagar <deshpandesagar187@gmail.com>
2024-05-22 14:11:52 +0530

    Added files

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const authRoutes = require('./routes/authenticate');
 
 const app = express();
 
 app.use(express.static('public'));
+app.use(authRoutes);
 
 app.set('view engine', 'ejs');
 
-const dbURI = "mongodb+srv://deshpandesagar15613:DvFetdrc7CFIdrZ8@cluster0.pyrmyxf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const dbURI = `mongodb+srv://deshpandesagar15613:oGzEbxyvGaJazNDD@authentication.pvbaqd7.mongodb.net/?retryWrites=true&w=majority&appName=Authentication`;
 
-mongoose.connect(dbURI);
+mongoose.connect(dbURI)
+.then(() => {
+    console.log("DB Connected Successfully...");
+})
+.catch((err) => {
+    console.log(err);
+});
 
 const PORT = 5000 || process.env.PORT;
 
 app.listen(PORT, ()=> {
     console.log(`Listening on server port: ${PORT}`);
 });
-
-
 
