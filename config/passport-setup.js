9c21fc3f93097dde4ae71ba59feb33ad8ad47b7d
Sagar <deshpandesagar187@gmail.com>
2024-05-29 15:43:55 +0530

    New Changes

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,11 +1,10 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
-const keys = require('./keys');
 
 passport.use(
     new GoogleStrategy({
-        clientID: keys.google.clientID,
-        clientSecret: keys.google.clientSecret,
+        clientID: process.env.CLIENTID,
+        clientSecret: process.env.CLIENTSECRET,
     }), () => {
 
     }
